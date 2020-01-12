e0f49625c1a6a13d0ca1558f9b14d38277b10000
ALEXANDR IVACHSHIN <44602298+iv86@users.noreply.github.com>
2020-01-12 21:09:32 +0600

    commit1

---
+++ src/personalfinance/model/Common.java
@@ -0,0 +1,15 @@
+package personalfinance.model;
+
+abstract public class Common {
+
+    public String getValueForComboBox() {
+        return null;
+    }
+
+
+
+    public void postAdd() {}
+    public void postEdit() {}
+    public void postRemove() {}
+
+}
